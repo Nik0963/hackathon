04ba3723a78ca8cd76a1d71ec89d88411d5b1384
Nik0963 <Nikhilpatil2401@gmail.com>
2023-11-17 15:19:01 +0530

    second commit

--- MovieReview/src/tester/TestApp.java
+++ MovieReview/src/tester/TestApp.java
@@ -1,12 +1,73 @@
 package tester;
 
+import java.util.List;
 import java.util.Scanner;
 
+import com.app.DateUtil;
+import com.app.MovieDao;
+import com.app.Movies;
 import com.app.UserDao;
 import com.app.Users;
 
 public class TestApp {
 	public static Scanner sc = new Scanner(System.in);
+
+	public static void userOperations() {
+		int choice;
+		Users curUser = null;
+		do {
+			System.out.print("\n1. Sign Up\n2. Sign In\nEnter choice: ");
+			choice = sc.nextInt();
+			switch (choice) {
+			case 0: // Exit
+				System.out.println("Bye!");
+				break;
+			case 1: // Sign Up
+				signUp();
+				break;
+			case 2: // Sign In
+				curUser = signIn();
+				System.out.println("Sign In successfull!");
+				adminOperations(curUser);
+				break;
+			}
+		} while (choice != 0);
+	}
+
+	public static void adminOperations(Users curUser) {
+		int choice;
+		do {
+			System.out.print(
+					"\n0. Log out\n1. Edit Profile\n2. Change Password\n3. Write Review\n4. Edit Review\n5. Display All Movies\n6. Display all reviews\n7. Display my reviews\n8. Display reviews shared with me\n9. Share a review\n10. Delete a review\nEnter choice: ");
+			choice = sc.nextInt();
+			switch (choice) {
+			case 0: // Log out
+				System.out.println("Bye!");
+				break;
+			case 1: // Edit Profile
+
+				break;
+			case 2: // Change Password
+				break;
+			case 3: // Write Review
+				break;
+			case 4: // Edit Review
+				break;
+			case 5: // Display All Movies
+				break;
+			case 6: // Display all reviews
+				break;
+			case 7: // Display my reviews
+				break;
+			case 8: // Display reviews shared with me
+				break;
+			case 9: // Share a review
+				break;
+			case 10: // Delete a review
+				break;
+			}
+		} while (choice != 0);
+	}
 
 	public static void signUp() {
 		System.out.print("First Name: ");
@@ -15,14 +76,16 @@ public class TestApp {
 		String lname = sc.next();
 		System.out.print("Email: ");
 		String email = sc.next();
+		System.out.print("Mobile: ");
+		String mobile = sc.next();
 		System.out.print("Password: ");
 		String passwd = sc.next();
 		System.out.print("Birth Date (dd-MM-yyyy): ");
 		String dateStr = sc.next();
-		Users voter = new Users();
+		Users user = new Users(0, fname, lname, email, passwd, mobile, DateUtil.parse(dateStr));
 		try (UserDao dao = new UserDao()) {
-			int count = dao.save(voter);
-			System.out.println("Voter registered: " + count);
+			int count = dao.save(user);
+			System.out.println("User registered: " + count);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -43,6 +106,34 @@ public class TestApp {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	public static void displayAllMovies() {
+		try (MovieDao dao = new MovieDao()) {
+			List<Movies> list = dao.displayAllMovies();
+			list.forEach(System.out::println);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void editProfile(Users user) {
+		try (UserDao dao = new UserDao()) {
+			int count = dao.editProfile(user);
+			System.out.println("Rows affetcted: " + count);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void changePassword(Users user) {
+
+		try (UserDao dao = new UserDao()) {
+			int count = dao.changePassword(user);
+			System.out.println("Rows affected: " + count);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
